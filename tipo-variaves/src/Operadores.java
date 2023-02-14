f684ce5c97c79ac9a1685326010c8e8ad04d5dd4
Pedream <pedreamsaraiva@gmail.com>
2023-02-14 20:52:51 -0300

    Operadores logicos

--- tipo-variaves/src/Operadores.java
+++ tipo-variaves/src/Operadores.java
@@ -7,6 +7,13 @@ public class Operadores {
         int numero = 5; 
         System.out.println(numero ++); // ++ = a adicionar mais um //-- = a subitrair menos um // para que seja adcionado na impressão ele deve ser colocado na antes da "variavel" exempro (++/-- numero)
 // boolean = false/treu
+
+// operadores unitarios 
+//(+) Operador unário de valor positivo – números são positivos sem esse operador explicitamente;
+//(-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+//(++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+//(--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+//(!) Operador unário lógico de negação – nega o valor de uma expressão booleana;
         boolean variavel = true;
         System.out.println(!variavel); // ! antes da variavel boolean inverta o valor
         System.out.println(variavel);  //temporariamente// serve tbm pro ++ 
@@ -30,7 +37,12 @@ public class Operadores {
     System.out.println(resultado);
 
     // Relacionais 
-
+    //== Quando desejamos verificar se uma variável é IGUAL A outra.
+    //!= Quando desejamos verificar se uma variável é DIFERENTE da outra.
+    //> Quando desejamos verificar se uma variável é MAIOR QUE a outra.
+    //>= Quando desejamos verificar se uma variável é MAIOR OU IGUAL a outra.
+    //< Quando desejamos verificar se uma variável é MENOR QUE outra.
+    //<= Quando desejamos verificar se uma variável é MENOR OU IGUAL a outra.
     int numero1, numero2;
 
     numero1 = 1;
@@ -47,6 +59,21 @@ public class Operadores {
 
     simNao = numero1 < numero2;
     System.out.println("Numero1 é menor que o numero 2 "  + simNao); 
+
+    // Operadores logicos
+   //  && Operador Lógico "E"
+    // || Operador Lógico "OU"
+
+    boolean condicao1 = true;
+    boolean condicao2 = false;
+
+    if(condicao1 && condicao2){
+        System.out.println("As duas condiçoes sao verdadeiras ");
+    }
+
+    if(condicao1 || condicao2){
+        System.out.println(" Uma das condiçoes é verdadeira ");
+    }
 
 
 
