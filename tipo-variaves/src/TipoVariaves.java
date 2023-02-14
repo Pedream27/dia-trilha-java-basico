22f84f73a6d77019ae9f9433fc522b04db78814d
Pedream <pedreamsaraiva@gmail.com>
2023-02-14 19:42:35 -0300

    Tipos de variaves primitivas

--- tipo-variaves/src/TipoVariaves.java
+++ tipo-variaves/src/TipoVariaves.java
@@ -1,3 +1,21 @@
 public class TipoVariaves {
+ // Toda classe deve ter sua iniciais em CAIXA ALTA
+    public static void main(String[] args) throws Exception {
+
+         Double salarioMinimo = 2.500;
+         Short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal;
+         int numero = 5;
+         numero = 10; 
+         System.out.println(numero);
+         final double VALOR_DO_PI = 3.18;
+         // Toda variavel deve ter em caixa alta apenas a inicial da segunda palavra 
+         //  VALOR_DO_PI = 15; nao tem como alterar final e ele tem q ser todo em CAIXA ALTA 
+
+
+
+        
+    }
     
 }
